8b6d16e28e7c8acb5ea0f4ecf0446d9768df59db
Edward Paget <edward@cassetteta.pe>
2014-10-07 22:43:58 -0500

    Remove Frontend Components. Close #1

--- project.clj
+++ project.clj
@@ -5,8 +5,6 @@
   :license {:name "GNU Affero General Public License v3"
             :url "https://www.gnu.org/licenses/agpl-3.0.html"}
 
-
-
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
                  [http-kit "2.1.18"]
                  [compojure "1.1.9"]
@@ -17,32 +15,9 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [com.stuartsierra/component "0.2.2"]
                  [org.clojure/tools.nrepl "0.2.5"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/clojurescript "0.0-2342"]
-                 [markdown-clj "0.9.53"]
-                 [jayq "2.5.2"]]
-
-  :plugins [[lein-cljsbuild "1.0.3"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :profiles {:uberjar {:main replme.core
-                       :aot [replme.core]
-                       :hooks [leiningen.cljsbuild]}
+                       :aot [replme.core]}
              :dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.3"]]}}
-  :cljsbuild {
-             :builds {:prod {:source-paths ["src"]
-                              :compiler {:preamble ["libs/jquery-1.10.2.min.js"
-                                                    "libs/jquery.console.js"]
-                                         :externs ["libs/jquery-1.10.2.min.js"
-                                                   "libs/jquery.console.js"]
-                                         :output-to "resources/public/replme.js"
-                                         :output-dir "resources/public/out"
-                                         :pretty-print false
-                                         :optimizations :advanced
-                                         :closure-warnings {:externs-validation :off
-                                                            :non-standard-jsdoc :off}}}
-                       :dev {:source-paths ["src"]
-                             :compiler {
-                                        :output-to "resources/public/replme.js"
-                                        :optimizations :none
-                                        :source-map true}}}})
+                   :dependencies [[org.clojure/tools.namespace "0.2.3"]]}})
